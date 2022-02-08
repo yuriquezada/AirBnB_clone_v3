c659de911b0a131c2c4b3cef85a94df3acdc0088
nitaly31 <geraldine.nitaly@gmail.com>
2022-02-07 20:29:44 -0800

    Update files

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """app.py to connect to API"""
 import os
 from models import storage
-from api.v1.views import app_views
+from api.v1.views.index import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
